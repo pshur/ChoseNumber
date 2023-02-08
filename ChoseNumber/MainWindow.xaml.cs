2b92923b1aeec88015555f854fff9a167121a0ab
pshur <pshur@ibagroup.eu>
2023-02-08 20:46:35 +0300

    add main form, validation

---
+++ ChoseNumber/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ChoseNumber
+{
+    public partial class MainWindow : Window
+    {
+        private bool _validName = false;
+        private bool _validAge = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitFields();
+        }
+
+        private void InitFields()
+        {
+            comboBoxSex.Items.Clear();
+            comboBoxSex.Items.Add(Gender.Male);
+            comboBoxSex.Items.Add(Gender.Female);
+            comboBoxSex.SelectedItem = Gender.Male;
+            textBoxAge.Text = "";
+            textBoxAge.BorderBrush = Brushes.Black;
+            textBoxName.Text = "";
+            textBoxName.BorderBrush = Brushes.Black;
+            _validName = false;
+            _validAge = false;
+        }
+
+        private void textBoxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool valid = string.IsNullOrEmpty(textBoxName.Text as string) || textBoxName.Text.Length < 10;
+            textBoxName.BorderBrush = !valid ? Brushes.Green : Brushes.Red;
+            _validName = !valid;
+        }
+
+        private void textBoxAge_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int invalid;
+            bool valid = int.TryParse(textBoxAge.Text, out invalid);
+            int value = valid ? int.Parse(textBoxAge.Text) : 0;
+            textBoxAge.BorderBrush = value > 0 ? Brushes.Green : Brushes.Red;
+            _validAge = value > 0;
+        }
+
+        private void startGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (_validAge && _validName)
+            {
+                UserScore score = new UserScore(textBoxName.Text, (Gender)comboBoxSex.SelectedItem, int.Parse(textBoxAge.Text));
+            } else
+            {
+                MessageBox.Show("Поля заполнены некорректно!");
+            }
+        }
+
+        private void showResults_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ChoseNumber/UserScore.cs
@@ -0,0 +1,32 @@
+﻿namespace ChoseNumber
+{
+    public class UserScore
+    {
+        public string UserName { get; } = "";
+        public char Sex { get; }
+        public int Age { get; }
+        public int[] Score  { get; } = new int[5];
+
+        public UserScore(string _name, Gender gender, int _age)
+        {
+            UserName = _name;
+            Sex = gender == Gender.Male ? 'М' : 'Ж';
+        }
+
+        public int AllScore()
+        {
+            int sum = 0;
+            for (int i = 0; i < Score.Length; i++) 
+            {
+                sum += Score[i];
+            }
+            return sum;
+        }
+
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
